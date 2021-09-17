3abda603ca28a11b79c745ac2ca00150e13c1fb6
Bagus TRIANURDIN <btstargroup@gmail.com>
2021-09-17 11:26:08 +0700

    fix some problem and try to build

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -6,7 +6,7 @@ const router = Router();
 
 router.get("/", function (req: Request, res: Response) {
   res.status(200).json({
-    message: "Welcome to KBBI Unofficial API",
+    message: "Welcome to New KBBI API",
     developer: "https://github.com/btrianurdin",
     endpoint: "/cari/[kosa kata]",
     example: `${Config.baseUrl}/cari/demokrasi`,
